6587e0287d3d555e4c1ed4b7064a55cfef9aa2b7
CesarSanchez19 <2302073@utrivieramaya.edu.mx>
2024-10-23 09:02:08 -0500

    second commit (Errores Visuales y Descripcion)

---
+++ README.md
@@ -0,0 +1,11 @@
+--- Practica de Java Script
+
+//Introduccion basica del lenguaje de programacion JavaScript
+
+// Tipos de datos
+
+// Variable
+
+//Helloword
+
+//Conexion con HTML
--- js/variable.js
+++ js/variable.js
@@ -1,14 +1,14 @@
-//Declara las variables
+// Declara las variables
 
-let nombre = 'cesar'; //String
+let nombre = 'cesar'; // String
 
-let estatus = true; //booleano
+let estatus = true; // booleano
 
-let city; //Undefined = indefinido
+let city; // Undefined = indefinido
 
 let address = null; // null = nulo
 
-//Declara variables sin datos
+// Declara variables sin datos
 
 let materia; // sin datos asignados
 
@@ -18,7 +18,7 @@ let materia3; // sin datos asignados
 
 let materia4; // sin datos asignados
 
-//Declara variables con datos asignados
+// Declara variables con datos asignados
 
 let profesor1 = 'Pino';
 
@@ -28,32 +28,29 @@ let profesor3 = 'Albino';
 
 let profesor4 = 'Perla';
 
-//Declara varibles para almacenar datos personales
+// Declara variables para almacenar datos personales
 
-let MyName = 'cesar'; //Mi nombre = String
+let MyName = 'cesar'; // Mi nombre = String
 
 let MyLastName = 'Sanchez'; // Mi apellido = String
 
-let isMaried = false; // Estas casado? = Booleano
+let isMarried = false; // ¿Estás casado? = Booleano
 
-let country = 'Mexico'; // Pais = string
+let country = 'Mexico'; // País = String
 
-let Age = 19; //Edad = intered
-
+let Age = 19; // Edad = Integer
 
 // Declare variables para almacenar su nombre, apellido, estado civil, país y edad en varias líneas.
-let myName1, MyLastName1, isMaried1, country1, age1;
+let myName1, MyLastName1, isMarried1, country1, age1;
 
+myName1 = 'David';
+MyLastName1 = 'Cecilia'; 
+age1 = 'Sebastian';
 
-name1 = 'David';
-name1 = 'Ceciliar';
-name1 = 'Sebastian';
-
-//Declare dos variables myAge y yourAge, asígneles valores iniciales e inicie sesión en la consola del navegador.
+// Declare dos variables myAge y yourAge, asígneles valores iniciales e inicie sesión en la consola del navegador.
 let myAge = 19;
 let yourAge = 25;
 
-//Concatenar un codigo en
+// Concatenar un código en
 
-console.log( 'Mi edad es' + ' ' + myAge, 'Tu edad es' + ' ' + yourAge)
-
+console.log('Mi edad es ' + myAge + ', Tu edad es ' + yourAge);
